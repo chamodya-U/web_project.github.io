6c33c6288c77ea4c30da56f589e17fc7239c4fd2
Chamodya <chamodyaUmayanga123@gmail.com>
2026-06-13 14:23:06 +0530

    book update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import {book as imgs} from "databse.js"
-
+import books from "./database.js";
 
 const params = new URLSearchParams(window.location.search);
 const id = parseInt(params.get("id"));
@@ -8,25 +7,36 @@ const similarBookContainer = document.querySelector(".similar-book-container");
 
 let mainBookImg = document.getElementById("main-book");
 let mainBookTitle = document.getElementById("book-title");
+let description = document.getElementById("book-description");
+let auhthor = document.getElementById("author");
+let pages = document.getElementById("pages");
+let published = document.getElementById("published");
+let language = document.getElementById("language");
 
-const book = imgs.find((book) => book.id === id);
+const book = books.find((book) => book.id === id);
 
 if (book) {
-  mainBookImg.src = book.address;
-  mainBookTitle.textContent = book.bookName;
+  mainBookImg.src = book.coverPage;
+  mainBookTitle.textContent = book.name;
+  description.innerText = book.description;
+  auhthor.innerHTML = `by <b>${book.author}</b>`;
+  pages.innerHTML = `pages <b>${book.numberOfPages}</b>`;
+  published.innerHTML = `published <b>${book.publishDate}</b>`;
+  language.innerHTML = `Language <b>${book.language}</b>`;
 }
 
-imgs.forEach((obj) => {
-  let name = obj.bookName;
-  let address = obj.address;
+books.forEach((obj) => {
+  let name = obj.name;
+  let coverPage = obj.coverPage;
   let index = obj.id;
 
-  if (!mainBookImg.src.includes(address)) {
+  //generating boook divs
+  if (!mainBookImg.src.includes(coverPage)) {
     let img = document.createElement("img");
     let card = document.createElement("div");
     let title = document.createElement("p");
     let link = document.createElement("a");
-    img.src = `${address}`;
+    img.src = `${coverPage}`;
     img.className = "card-img";
     link.href = `book.html?id=${index}`;
     title.className = "card-title";
@@ -44,10 +54,15 @@ const imgcards = document.querySelectorAll(".book-card");
 
 imgcards.forEach((card) => {
   card.addEventListener("click", () => {
-    obj = imgs.find((obj) => obj.id === parseInt(card.id.split("-")[2]));
+    const obj = books.find((obj) => obj.id === parseInt(card.id.split("-")[2]));
     console.log(obj);
 
-    mainBookImg.src = obj.address;
-    mainBookTitle.innerText = obj.bookName;
+    mainBookImg.src = obj.coverPage;
+    mainBookTitle.innerText = obj.name;
+    description.innerText = obj.description;
+    auhthor.innerHTML = `by <b>${obj.author}</b>`;
+    pages.innerHTML = `pages <b>${obj.numberOfPages}</b>`;
+    published.innerHTML = `published <b>${obj.publishDate}</b>`;
+    language.innerHTML = `Language <b>${obj.language}</b>`;
   });
 });
