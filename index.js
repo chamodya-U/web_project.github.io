95934a07308528581eda952ddc8c6d6b8e860c1c
Poojitha Pasindu <poojithapasindu@Poojithas-MacBook-Air.local>
2026-06-14 17:46:32 +0530

    lod out button

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const role = sessionStorage.getItem("role");
-
+$("#loginOut").hide();
 $("#addbook").hide();
 if (role === "admin") {
   $("#addbook").show();
+  $("#signLogin1").hide();
+  $("#loginOut").show();
 }
-if (role === "user") {
-  $("#addbook").hide();
+if(role === "user") {
+    $("#signLogin1").hide();
+    $("#loginOut").show();
 }
+
+$("#loginOut").click(() => {
+  sessionStorage.clear();
+  window.location.href = "login.html";
+});
